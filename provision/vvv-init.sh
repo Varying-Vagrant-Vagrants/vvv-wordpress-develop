ec6e7730ea6ad542138aca818e6be28be9f88b82
Weston Ruter <weston@xwp.co>
2017-03-18 15:47:53 -0700

    Add tests.wordpress-develop.dev domain to connect with unit testing DB

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -32,12 +32,12 @@ if [[ ! -d "${VVV_PATH_TO_SITE}/public_html" ]]; then
   cd ${VVV_PATH_TO_SITE}/public_html/src/
   echo "Creating wp-config.php for src.wordpress-develop.dev and build.wordpress-develop.dev."
   noroot wp core config --dbname=wordpress_develop --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
-// Match any requests made via xip.io.
-if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(src|build)(.wordpress-develop.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
+// Match any requests made via xip.io or tests.wordpress-develop.dev.
+if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^tests\.wordpress-develop\.dev$|^(src|build)(\.wordpress-develop\.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
     define( 'WP_HOME', 'http://' . \$_SERVER['HTTP_HOST'] );
     define( 'WP_SITEURL', 'http://' . \$_SERVER['HTTP_HOST'] );
 } else if ( 'build' === basename( dirname( __FILE__ ) ) ) {
-// Allow (src|build).wordpress-develop.dev to share the same Database
+    // Allow (src|build).wordpress-develop.dev to share the same Database
     define( 'WP_HOME', 'http://build.wordpress-develop.dev' );
     define( 'WP_SITEURL', 'http://build.wordpress-develop.dev' );
 }
@@ -45,9 +45,16 @@ if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(src|build)(.wordpress-dev
 define( 'WP_DEBUG', true );
 PHP
 
+  # Route WordPress to the tests database when requested.
+  replace "'DB_NAME'," "'DB_NAME', isset( \$_SERVER['HTTP_HOST'] ) && 0 === strpos( \$_SERVER['HTTP_HOST'], 'tests.' ) ? 'wordpress_unit_tests' : " -- wp-config.php
+  replace '$table_prefix =' "\$table_prefix = isset( \$_SERVER['HTTP_HOST'] ) && 0 === strpos( \$_SERVER['HTTP_HOST'], 'tests.' ) ? 'wptests_' : " -- wp-config.php
+
   echo "Installing src.wordpress-develop.dev."
   noroot wp core install --url=src.wordpress-develop.dev --quiet --title="WordPress Develop" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
   cp /srv/config/wordpress-config/wp-tests-config.php ${VVV_PATH_TO_SITE}/public_html/
+
+  replace "'example.org'" "'tests.wordpress-develop.dev'" -- ${VVV_PATH_TO_SITE}/public_html/wp-tests-config.php
+
   cd ${VVV_PATH_TO_SITE}/public_html/
 
 else
